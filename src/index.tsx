e3828785f07e31396cb07077aff325feb7280aa3
daniilcom <dan19.95@mail.ru>
2024-04-08 15:50:18 +0300

    setup: test env, fix:.gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from 'app/App';
-import ThemeProvider from 'app/providers/ThemeProvaider/ui/ThemeProvider';
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from 'app/App'
+import ThemeProvider from 'app/providers/ThemeProvaider/ui/ThemeProvider'
+import { ErrorBoundary } from 'app/providers/ErrorBoundary'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById('root')
+const root = createRoot(container)
 
 root.render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
-);
+  <BrowserRouter>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
+  </BrowserRouter>
+)
